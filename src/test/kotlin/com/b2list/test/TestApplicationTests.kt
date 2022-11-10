ab6b1f8ca76be0d2014808b2ac46267bcb3a1bce
Thales Belinazzi <thales@lighthost.com.br>
2022-11-10 19:13:17 -0300

    Created base project with spring initializr

---
+++ src/test/kotlin/com/b2list/test/TestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.b2list.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
