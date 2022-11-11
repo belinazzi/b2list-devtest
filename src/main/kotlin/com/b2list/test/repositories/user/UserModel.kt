cb2130ddff9d001cd95bf47277947322a3601ff6
Thales Belinazzi <thales@lighthost.com.br>
2022-11-11 15:00:20 -0300

    Implemented user model and repository

---
+++ src/main/kotlin/com/b2list/test/repositories/user/UserModel.kt
@@ -0,0 +1,29 @@
+package com.b2list.test.repositories.user
+
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tests_users")
+data class UserModel(
+    @Id
+    var cpf: Long,
+
+    @Column(name = "username", updatable = false, insertable = true, unique = true, nullable = false, length = 80)
+    var username: String,
+
+    @Column(name = "email", nullable = false, insertable = true, updatable = true, length = 260)
+    var email: String,
+
+    @Column(name = "name", nullable = false, insertable = true, updatable = true, length = 50)
+    var name: String,
+
+    @Column(name = "last_name", nullable = false, insertable = true, updatable = true, length = 100)
+    var lastName: String,
+
+    @Column(name = "created_at", nullable = false)
+    var creationDate:LocalDateTime = LocalDateTime.now()
+)
---
+++ src/main/kotlin/com/b2list/test/repositories/user/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.b2list.test.repositories.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
+
+@Repository
+interface UserRepository:JpaRepository<UserModel, Long> {
+    fun findByUsername(username:String):Optional<UserModel>
+}
