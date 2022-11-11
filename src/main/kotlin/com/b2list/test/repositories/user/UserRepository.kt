8d3c4e821ccb6f25af1defaa809555bcaf1cbb80
Thales Belinazzi <thales@lighthost.com.br>
2022-11-11 16:32:22 -0300

    Created method for cpf search

--- src/main/kotlin/com/b2list/test/repositories/user/UserRepository.kt
+++ src/main/kotlin/com/b2list/test/repositories/user/UserRepository.kt
@@ -7,4 +7,5 @@ import java.util.*
 @Repository
 interface UserRepository:JpaRepository<UserModel, Long> {
     fun findByUsername(username:String):Optional<UserModel>
+    fun findByCpf(cpf: Long):Optional<UserModel>
 }
