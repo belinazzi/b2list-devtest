82fe9670851b38c4018b5ab38067a88a3d220625
Thales Belinazzi <thales@lighthost.com.br>
2022-11-12 15:25:33 -0300

    Refactored code for swagger integration and Json type object remove

---
+++ src/main/kotlin/com/b2list/test/repositories/user/UserUpdateModel.kt
@@ -0,0 +1,6 @@
+package com.b2list.test.repositories.user
+
+data class UserUpdateModel (
+    var name:String,
+    var lastName:String,
+    var email:String)
--- src/main/kotlin/com/b2list/test/web/UserRestController.kt
+++ src/main/kotlin/com/b2list/test/web/UserRestController.kt
@@ -4,62 +4,68 @@ import br.com.colman.simplecpfvalidator.isCpf
 import com.b2list.test.repositories.user.UserActionResponseModel
 import com.b2list.test.repositories.user.UserModel
 import com.b2list.test.repositories.user.UserRepository
+import com.b2list.test.repositories.user.UserUpdateModel
 import com.b2list.test.utils.Utilities
-import org.json.JSONObject
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
 
 @RestController
+@ApiResponses(value = [ApiResponse(code = 500, message = "Internal Server Error")])
+@ApiModel(discriminator = "TESTE")
 class UserRestController {
 
     @Autowired
     lateinit var repository: UserRepository
 
-    @RequestMapping("/api/users/all")
-    fun findAll(remote:HttpServletRequest): Any? {
+    @GetMapping("/api/users/all")
+    @Operation(summary = "Get all registered users", description = "Return an array with all users data")
+    fun findAll(remote:HttpServletRequest): List<UserModel?>? {
         return try{
             repository.findAll()
         }catch (ex:Exception){
             println("API ERROR: /api/users/all caused by ${ex.localizedMessage} : FROM ${remote.remoteAddr}")
-            JSONObject(UserActionResponseModel(true, mutableListOf("Invalid search attribute."))).toMap()
+            null
         }
     }
 
-    @RequestMapping("/api/users/get/{cpf}")
-    fun findByCpf(@PathVariable cpf: String, remote:HttpServletRequest): Any?{
+    @GetMapping("/api/users/get/{cpf}")
+    @Operation(summary = "Get registered user data", description = "Return user data")
+    fun findByCpf(@PathVariable cpf: String, remote:HttpServletRequest): UserModel?{
         return try{
             repository.findByCpf(cpf.toLong()).orElse(null)
         }catch (ex:Exception){
             println("API ERROR: /api/users/get/$cpf caused by ${ex.localizedMessage} : FROM ${remote.remoteAddr}")
-            JSONObject(UserActionResponseModel(true, mutableListOf("Invalid search attribute."))).toMap()
+            null
         }
     }
 
     @PostMapping(path = ["/api/users/create"], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun createUser(@RequestBody json: String?, remote:HttpServletRequest): Any?
+    @Operation(summary = "Register new user", description = "Create new user")
+    fun createUser(@RequestBody data: UserModel, remote:HttpServletRequest): UserActionResponseModel
     {
         var error = false
         val errorItems = mutableListOf<String>()
 
         try {
-            val data = JSONObject(json)
+            val cpf = data.cpf
+            if(!cpf.isCpf()){ error = true; errorItems.add("cpf-invalid") }
 
-            var cpf = data["cpf"].toString()
-            if(!cpf.isCpf()){ error = true; errorItems.add("cpf-invalid")
-            } else{ cpf = cpf.replace(".", "").replace("-", "") }
-
-            val username = data["username"].toString().replace("\\s".toRegex(), "")
+            val username = data.username.replace("\\s".toRegex(), "")
             if(username.length < 3){error = true; errorItems.add("username-lenght")}
 
-            val email = data["email"].toString().lowercase()
+            val email = data.email.lowercase()
             if(!"^[A-Za-z](.*)(@)(.+)(\\.)(.+)".toRegex().matches(email)){ error = true; errorItems.add("email-invalid") }
 
-            val name = Utilities.capitalizeString(data["name"].toString().lowercase())
+            val name = Utilities.capitalizeString(data.name.lowercase())
             if(name.length < 3){error = true; errorItems.add("name-lenght")}
 
-            val lastName = Utilities.capitalizeString(data["last_name"].toString())
+            val lastName = Utilities.capitalizeString(data.lastName)
             if(lastName.length < 3){error = true; errorItems.add("last_name-lenght")}
 
             repository.findByUsername(username).ifPresent {
@@ -67,25 +73,26 @@ class UserRestController {
                 errorItems.add("username-duplicated")
             }
 
-            repository.findByCpf(cpf.toLong()).ifPresent {
+            repository.findByCpf(cpf).ifPresent {
                 error = true
                 errorItems.add("cpf-duplicated")
             }
 
             if(!error){
-                repository.save(UserModel(cpf.toLong(), username, email, name, lastName))
+                repository.save(UserModel(cpf, username, email, name, lastName))
             }
 
-            return JSONObject(UserActionResponseModel(error, errorItems)).toMap()
+            return UserActionResponseModel(error, errorItems)
         }
         catch (ex:Exception){
             println("API ERROR: /api/users/create caused by ${ex.localizedMessage} : FROM ${remote.remoteAddr}")
-            return JSONObject(UserActionResponseModel(true, mutableListOf("An Error ocurred on creation proccess."))).toMap()
+            return UserActionResponseModel(true, mutableListOf("An Error ocurred on creation proccess."))
         }
     }
 
     @DeleteMapping(path = ["/api/users/delete/{cpf}"])
-    fun deleteUser(remote:HttpServletRequest, @PathVariable cpf: String): Any?
+    @Operation(summary = "Delete user", description = "Delete registered user")
+    fun deleteUser(remote:HttpServletRequest, @PathVariable cpf: String): UserActionResponseModel
     {
         var error = false
         val errorItems = mutableListOf<String>()
@@ -99,27 +106,27 @@ class UserRestController {
         }
         catch (ex:Exception){
             println("API ERROR: /api/users/delete/$cpf caused by ${ex.localizedMessage} : FROM ${remote.remoteAddr}")
-            return JSONObject(UserActionResponseModel(true, mutableListOf("An Error ocurred on creation proccess."))).toMap()
+            error = true
+            errorItems.add("An Error ocurred on creation proccess.")
         }
         return UserActionResponseModel(error, errorItems)
     }
 
     @PatchMapping(path = ["/api/users/update/{cpf}"])
-    fun updateUser(@RequestBody json: String?, remote:HttpServletRequest, @PathVariable cpf: String): Any?
+    @Operation(summary = "Update user", description = "Update user data")
+    fun updateUser(@RequestBody updateData: UserUpdateModel, remote:HttpServletRequest, @PathVariable cpf: String): UserActionResponseModel
     {
         var error = false
         val errorItems = mutableListOf<String>()
 
         try {
-            val updateData = JSONObject(json)
-
-            val email = updateData["email"].toString().lowercase()
+            val email = updateData.email.lowercase()
             if(!"^[A-Za-z](.*)(@)(.+)(\\.)(.+)".toRegex().matches(email)){ error = true; errorItems.add("email-invalid") }
 
-            val name = Utilities.capitalizeString(updateData["name"].toString().lowercase())
+            val name = Utilities.capitalizeString(updateData.name)
             if(name.length < 3){error = true; errorItems.add("name-lenght")}
 
-            val lastName = Utilities.capitalizeString(updateData["last_name"].toString())
+            val lastName = Utilities.capitalizeString(updateData.lastName)
             if(lastName.length < 3){error = true; errorItems.add("last_name-lenght")}
 
             if(!error){
@@ -133,11 +140,11 @@ class UserRestController {
                     errorItems.add("This user does not exists.")
                 })
             }
-            return JSONObject(UserActionResponseModel(error, errorItems)).toMap()
+            return UserActionResponseModel(error, errorItems)
         }
         catch (ex:Exception){
             println("API ERROR: /api/users/update/$cpf caused by ${ex.localizedMessage} : FROM ${remote.remoteAddr}")
-            return JSONObject(UserActionResponseModel(true, mutableListOf("An Error ocurred on update proccess."))).toMap()
+            return UserActionResponseModel(true, mutableListOf("An Error ocurred on update proccess."))
         }
     }
 
